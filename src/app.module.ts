212b76f9a594e4bb07ec023bb3415a767a17dd7a
debabin <dmitriybabinup@gmail.com>
2023-01-31 11:25:39 +0700

    ☄️ wake up module + task

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule, Cron } from '@nestjs/schedule';
+import { Injectable, Logger } from '@nestjs/common';
 
 import { PizzaModule } from './pizza/pizza.module';
 import { RspModule } from './rsp/rsp.module';
+import { WakeUpModule } from './wakeup/wakeup.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PizzaModule, RspModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    ConfigModule.forRoot(),
+    PizzaModule,
+    RspModule,
+    WakeUpModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/wakeup/wakeup.service.ts
@@ -0,0 +1,24 @@
+import { Cron } from '@nestjs/schedule';
+import { ForbiddenException, Injectable, Logger } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { catchError, lastValueFrom, map } from 'rxjs';
+
+@Injectable()
+export class WakeUpService {
+  constructor(private readonly httpService: HttpService) {}
+
+  private readonly logger = new Logger(WakeUpService.name);
+
+  @Cron('* */20 * * * *')
+  async handleCron() {
+    const request = this.httpService
+      .get('https://shift-winter-2023-backend.onrender.com/api', {
+        headers: { 'Accept-Encoding': 'gzip,deflate,compress' },
+      })
+      .pipe(map((res) => res.status));
+
+    this.logger.debug('wake up');
+    const responseStatus = await lastValueFrom(request);
+    this.logger.debug(`status - ${responseStatus}`);
+  }
+}
